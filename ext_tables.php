efdc9d30ad0c7dc3652922598da5d6588ac1deaa
pfennigstorf <pfennigstorf@sub.uni-goettingen.de>
2011-11-23 15:09:26 +0000

    Suche im Backend TYPO3 4.6. kompatibel gestaltet (WWW-896)
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/w3d/branches/dev/typo3conf/ext/standorte@1350 f004c202-0a6e-0410-85e4-8d7485fa9ffe

--- ext_tables.php
+++ ext_tables.php
@@ -23,6 +23,7 @@ $TCA['tx_standorte_domain_model_fakultaet'] = array(
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . '/Configuration/TCA/tx_standorte_domain_model_fakultaet.php',
 		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . '/Resources/Public/img/icon_tx_standorte_domain_model_fakultaet.png',
+		'searchFields' => 'titel'
 	),
 );
 
@@ -43,6 +44,7 @@ $TCA['tx_standorte_domain_model_bibliothek'] = array(
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . '/Configuration/TCA/tx_standorte_domain_model_bibliothek.php',
 		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . '/Resources/Public/img/icon_tx_standorte_domain_model_bibliothek.png',
+		'searchFields' => 'titel, sigel'
 	),
 );
 
